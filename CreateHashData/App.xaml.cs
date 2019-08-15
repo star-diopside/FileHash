5726a4b273d1cde4c669a6cdc2b48015b38c6741
se-yoshi <se.yoshi@gmail.com>
2019-08-15 20:21:41 +0900

    Initial commit

---
+++ CreateHashData/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace CreateHashData
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+    }
+}
---
+++ CreateHashData/src/CreateHashData/View/MainView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using CreateHashData.ViewModel;
+
+namespace CreateHashData.View
+{
+    /// <summary>
+    /// MainView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+    }
+}
