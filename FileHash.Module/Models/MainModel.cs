3ef441a35d865185a9d0d6b8b02e17a82db99226
se-yoshi <se.yoshi@gmail.com>
2023-11-18 23:05:34 +0900

    .NET 8 に移行

--- FileHash.Module/Models/MainModel.cs
+++ FileHash.Module/Models/MainModel.cs
@@ -1,7 +1,7 @@
 ﻿using Reactive.Bindings;
+using System.Collections.Frozen;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -13,20 +13,28 @@ namespace FileHash.Module.Models
         {
         }
 
-        public ObservableCollection<string> SupportedHashAlgorithms { get; } =
-            new ObservableCollection<string>() { "MD5", "SHA1", "SHA256", "SHA384", "SHA512" };
+        public ObservableCollection<string> SupportedHashAlgorithms { get; } = ["MD5", "SHA1", "SHA256", "SHA384", "SHA512"];
 
-        public ReactivePropertySlim<string> HashAlgorithmName { get; } = new ReactivePropertySlim<string>("MD5");
+        public ReactivePropertySlim<string> HashAlgorithmName { get; } = new("MD5");
 
-        public ReactivePropertySlim<string> FileName { get; } = new ReactivePropertySlim<string>();
+        public ReactivePropertySlim<string> FileName { get; } = new();
 
-        public ReactivePropertySlim<string> ComputedFileHashString { get; } = new ReactivePropertySlim<string>();
+        public ReactivePropertySlim<string> ComputedFileHashString { get; } = new();
 
-        public ReactivePropertySlim<string> CompareFileHashString { get; } = new ReactivePropertySlim<string>();
+        public ReactivePropertySlim<string> CompareFileHashString { get; } = new();
+
+        private readonly FrozenDictionary<string, Func<HashAlgorithm>> HashAlgorithmStrategy = new Dictionary<string, Func<HashAlgorithm>>()
+        {
+            { "MD5", () => MD5.Create() },
+            { "SHA1", () => SHA1.Create() },
+            { "SHA256", () => SHA256.Create() },
+            { "SHA384", () => SHA384.Create() },
+            { "SHA512", () => SHA512.Create() }
+        }.ToFrozenDictionary();
 
         public void ComputeFileHash()
         {
-            using var algorithm = HashAlgorithm.Create(HashAlgorithmName.Value);
+            using var algorithm = HashAlgorithmStrategy[HashAlgorithmName.Value]();
             using var stream = new FileStream(FileName.Value, FileMode.Open, FileAccess.Read);
             var hash = algorithm.ComputeHash(stream);
             ComputedFileHashString.Value = hash.Select(b => b.ToString("x2"))
